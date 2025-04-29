8792114086938ab3d27e71362724eaefe31e6151
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-04-29 22:55:22 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Глобальні змінні
+selected_pairs = ["EURUSD=X"]
+analyzing = False
+last_signal = {}
+last_signal_time = {}
+
+pairs_list = {
+    "EUR/USD": "EURUSD=X",
+    "GBP/USD": "GBPUSD=X",
+    "USD/JPY": "USDJPY=X",
+    "AUD/USD": "AUDUSD=X",
+    "USD/CAD": "USDCAD=X",
+    "EUR/JPY": "EURJPY=X",
+    "GBP/JPY": "GBPJPY=X",
+    "EUR/GBP": "EURGBP=X",
+    "NZD/USD": "NZDUSD=X",
+    "USD/CHF": "USDCHF=X"
+}
