78b0bac969025c6a89adf004900fc466ce186ba7
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-01 12:53:21 +0300

    Update status_report.py

--- status_report.py
+++ status_report.py
@@ -2,8 +2,7 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 def status(update: Update, context: CallbackContext):
-    analyzing = context.bot_data.get("analyzing_ref", lambda: False)()
-    if analyzing:
+    if context.bot_data.get("analyzing_ref", lambda: False)():
         update.message.reply_text("Аналіз зараз *запущений*.", parse_mode="Markdown")
     else:
         update.message.reply_text("Аналіз зараз *не запущений*.", parse_mode="Markdown")
