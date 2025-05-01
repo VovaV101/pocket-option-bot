ad1b425d59d96830bef7c115b68b8e7cdd5fb6c8
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-01 12:52:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
-# config.py
-
-selected_pairs = []  # сюди додаються вибрані пари користувачем
-analyzing = False    # чи активний аналіз
-job_reference = None # посилання на задачу
-
-# Для контролю повторних сигналів
+selected_pairs = []
+analyzing = False
 last_signal = {}
 last_signal_time = {}
 
-# Валютні пари та тікери yfinance
 pairs_list = {
     "EUR/USD": "EURUSD=X",
     "GBP/USD": "GBPUSD=X",
@@ -17,5 +11,5 @@ pairs_list = {
     "AUD/USD": "AUDUSD=X",
     "NZD/USD": "NZDUSD=X",
     "EUR/JPY": "EURJPY=X",
-    "GBP/JPY": "GBPJPY=X",
+    "GBP/JPY": "GBPJPY=X"
 }
