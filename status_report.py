191dfd91a9447f4c6a7184f2be6d143533ee8de4
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-01 20:51:53 +0300

    Delete status_report.py

--- status_report.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from handlers import is_running
-
-def status(update: Update, context: CallbackContext):
-    if is_running:
-        update.message.reply_text("Аналіз зараз активний ✅", parse_mode="Markdown")
-    else:
-        update.message.reply_text("Аналіз зараз неактивний ❌", parse_mode="Markdown")
