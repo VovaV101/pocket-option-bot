94452aabc0ab2dc4acc774d8073f561430a2d7ff
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-01 18:10:10 +0300

    Update status_report.py

--- status_report.py
+++ status_report.py
@@ -4,6 +4,6 @@ from handlers import is_running
 
 def status(update: Update, context: CallbackContext):
     if is_running:
-        update.message.reply_text("Аналіз зараз активний ✅")
+        update.message.reply_text("Аналіз зараз активний ✅", parse_mode="Markdown")
     else:
-        update.message.reply_text("Аналіз зараз неактивний ❌")
+        update.message.reply_text("Аналіз зараз неактивний ❌", parse_mode="Markdown")
