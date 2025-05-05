ee789f5614dea29ef079c5ff9834e022b6fcc505
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-05 22:27:12 +0300

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -29,8 +29,11 @@ def pair_selected(update: Update, context: CallbackContext):
 def turn_on(update: Update, context: CallbackContext):
     global analyzing
     if not analyzing:
+        if not selected_pairs:
+            update.message.reply_text('Спочатку оберіть валютні пари командою /pairs!')
+            return
         update.message.reply_text('▶️ Аналіз увімкнено.')
-        start_analysis()
+        start_analysis(selected_pairs)
         analyzing = True
     else:
         update.message.reply_text('Аналіз уже увімкнений.')
