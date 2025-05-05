bf3f94f48da3f013fe87a4001d7cdb3d58bacea1
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-05 22:16:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
-import asyncio
 import os
-from queue import Queue
 from flask import Flask, request
-from telegram import Bot, Update
+from telegram import Update
 from telegram.ext import (
-    Application, ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext
+    ApplicationBuilder, CommandHandler, MessageHandler, filters
 )
 from src.config import TELEGRAM_TOKEN, WEBHOOK_URL
 from src.handlers import start, pairs, pair_selected, turn_on, turn_off
 from src.status_report import send_signal
 
-# Flask сервер
 app = Flask(__name__)
-
-# Ініціалізація Telegram бота через ApplicationBuilder
 application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-# Реєстрація обробників команд
+# Команди
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("pairs", pairs))
 application.add_handler(CommandHandler("on", turn_on))
@@ -24,7 +19,6 @@ application.add_handler(CommandHandler("off", turn_off))
 application.add_handler(CommandHandler("status", send_signal))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, pair_selected))
 
-# Flask маршрут для Webhook
 @app.route(f"/{TELEGRAM_TOKEN}", methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), application.bot)
@@ -35,11 +29,8 @@ def webhook():
 def home():
     return "Бот працює!"
 
-async def main():
-    await application.bot.delete_webhook()
-    await application.bot.set_webhook(url=f"{WEBHOOK_URL}/{TELEGRAM_TOKEN}")
+if __name__ == "__main__":
+    application.bot.delete_webhook()
+    application.bot.set_webhook(url=f"{WEBHOOK_URL}/{TELEGRAM_TOKEN}")
     print(f"Webhook встановлено: {WEBHOOK_URL}/{TELEGRAM_TOKEN}")
     app.run(host="0.0.0.0", port=8000)
-
-if __name__ == "__main__":
-    asyncio.run(main())
