7f6baeed306dcb3f5051986fcadee81066dd3e73
VovaV101 <138198132+VovaV101@users.noreply.github.com>
2025-05-07 00:41:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
-import asyncio
 from telegram.ext import ApplicationBuilder
 from src.handlers import setup_handlers
 from src.config import BOT_TOKEN
 
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     setup_handlers(app)
 
     print("Bot started and polling...")
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
